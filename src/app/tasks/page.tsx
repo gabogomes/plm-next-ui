33181941899396ba69e462099bf59f7a425fd119
Gabriel Gomes <aulasconsultorias@gmail.com>
2024-01-20 20:43:38 -0300

    feat: including table in the tasks page for viewing existing tasks.

--- src/app/tasks/page.tsx
+++ src/app/tasks/page.tsx
@@ -1,9 +1,11 @@
 'use client'
+import Link from 'next/link'
 import { useState } from 'react'
 import LoadingOverlay from 'react-loading-overlay-ts'
 import { mutate, SWRResponse } from 'swr'
 
 import TaskCreationModal from '@/components/tasks/TaskCreationModal' // Import your modal component here
+import { ITasksResponse } from '@/types'
 import useSwr from '@/utils/swr'
 import { useAuth } from '@clerk/nextjs'
 
@@ -35,12 +37,34 @@ const TasksPage = () => {
         Add Task
       </button>
       <LoadingOverlay className="h-100" active={taskIsLoading}>
-        <div>{JSON.stringify(taskData)}</div>
+        <table className={styles.tableContainer}>
+          <thead>
+            <tr>
+              <th className={styles.tableHeader}>Name</th>
+              <th className={styles.tableHeader}>Type</th>
+              <th className={styles.tableHeader}>Status</th>
+              <th className={styles.tableHeader}>Correspondence Email</th>
+            </tr>
+          </thead>
+          <tbody>
+            {!taskIsLoading &&
+              taskData.items.map((item: ITasksResponse) => (
+                <tr key={item.id}>
+                  <td className={styles.tableCell}>
+                    <Link href={`/tasks/${item.id as number}~${(item.name as string).toLowerCase().replaceAll(' ', '_')}`}>
+                      {item.name}
+                    </Link>
+                  </td>
+                  <td className={styles.tableCell}>{item.type}</td>
+                  <td className={styles.tableCell}>{item.status}</td>
+                  <td className={styles.tableCell}>{item.correspondenceEmailAddress}</td>
+                </tr>
+            ))}
+          </tbody>
+        </table>
       </LoadingOverlay>
-      
-      {isModalOpen && (
-        <TaskCreationModal closeModal={closeModal} />
-      )}
+  
+      {isModalOpen && <TaskCreationModal closeModal={closeModal} />}
     </div>
   )
 }
